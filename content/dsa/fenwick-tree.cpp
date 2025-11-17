e071a7e61e50214f4535a0fdaa8f0793cf44384b
Miguel <migueldco14@gmail.com>
2025-11-17 18:40:13 -0300

    minor adjustments

--- content/dsa/fenwick-tree.cpp
+++ content/dsa/fenwick-tree.cpp
@@ -8,15 +8,11 @@ struct Fenwick {
 	int n;
 	Fenwick(int N) : t(N), n(N) {}
 	void add(int i, int val) {
-		for (i++; i <= n; i += i & -i) {
-			t[i - 1] += val;
-		}
+		for (i++; i <= n; i += i & -i) t[i - 1] += val;
 	}
 	int sum(int r) {
 		int s = 0;
-		for (; r; r -= r & -r) {
-			s += t[r - 1];
-		}
+		for (; r; r -= r & -r) s += t[r - 1];
 		return s;
 	}
 	int sum(int l, int r) {
