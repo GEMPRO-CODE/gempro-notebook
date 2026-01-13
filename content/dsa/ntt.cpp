1867d810712ae24bbfe413de3038fbb6533aaa53
Miguel <migueldco14@gmail.com>
2026-01-13 17:10:57 -0300

    reimplement ntt

--- content/dsa/ntt.cpp
+++ content/dsa/ntt.cpp
@@ -1,54 +1,61 @@
 #include <vector>
 #include <cstdint>
 #include <iostream>
+#include <array>
+#include <span>
 
 using namespace std;
 
-using i64 = int64_t;
+using i64 = long long;
 
 using VI = vector<int>;
-
-// Adapted from kactl
 
 // begin template //
 using VL = vector<i64>;
 
 const int MOD = 998244353, root = 62;
 
-i64 powm(i64 x, i64 e, i64 mod) {
+i64 powm(i64 x, i64 e) {
 	i64 r = 1;
 	while (e) {
-		if (e & 1) (r *= x) %= mod;
+		if (e & 1) (r *= x) %= MOD;
 		e >>= 1;
-		(x *= x) %= mod;
+		(x *= x) %= MOD;
 	}
 	return r;
 }
 
 void ntt(VL &a) {
-	int n = a.size(), L = 31 - __builtin_clz(n);
-	static VL rt(2, 1);
-	for (static int k = 2, s = 2; k < n; k *= 2, s++) {
-		rt.resize(n);
-		i64 z[] = {1, powm(root, MOD >> s, MOD)};
-		for (int i = k; i < 2 * k; i++) rt[i] = rt[i / 2] * z[i & 1] % MOD;
+	int n = a.size(), k = bit_width(unsigned(n)) - 1;
+	static array<i64, 1 << 23> r = {1, 1};
+	for (static int t = 2, s = 2; t < n; t <<= 1, s++) {
+		array z = {1ll, powm(root, MOD >> s)};
+		for (int i = t; i < t << 1; i++) r[i] = r[i >> 1] * z[i & 1] % MOD;
 	}
-	VI rev(n);
-	for (int i = 0; i < n; i++) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-	for (int i = 0; i < n; i++) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k <<= 1)
-		for (int i = 0; i < n; i += k << 1)
-			for (int j = 0; j < k; j++) {
-				i64 z = rt[j + k] * a[i + j + k] % MOD, &ai = a[i + j];
-				a[i + j + k] = ai - z + (z > ai ? MOD : 0);
-				ai += (ai + z >= MOD ? z - MOD : z);
+	static array<i64, 1 << 23> b, c;
+	copy(a.begin(), a.end(), b.begin());
+	for (int m = n; m >>= 1;) {
+		for (int l = 0; l < n; l += m << 1) 
+			for (int i = 0; i < m; i++) 
+				c[l + i] = b[l + 2 * i], c[l + m + i] = b[l + 2 * i + 1];
+		copy(c.begin(), c.begin() + n, b.begin());
+	}
+	for (int m = 1; m < n; m <<= 1) {
+		for (int l = 0; l < n; l += m << 1)
+			for (int i = 0; i < m; i++) {
+				i64 z = r[m + i] * b[l + m + i] % MOD;
+				c[l + i] = b[l + i] + z;
+				c[l + m + i] = b[l + i] + (MOD - z);
 			}
+		for (int i = 0; i < n; i++) b[i] = c[i] < MOD ? c[i] : c[i] - MOD;
+	}
+	copy(b.begin(), b.begin() + n, a.begin());
 }
+
 VL conv(VL &a, VL &b) {
 	if (a.empty() || b.empty()) return {};
-	int s = ssize(a) + ssize(b) - 1, B = 32 - __builtin_clz(s),
-	    n = 1 << B;
-	int inv = powm(n, MOD - 2, MOD);
+	int s = ssize(a) + ssize(b) - 1, B = 32 - __builtin_clz(s), n = 1 << B;
+	int inv = powm(n, MOD - 2);
 	VL L(a), R(b), out(n);
 	L.resize(n), R.resize(n);
 	ntt(L), ntt(R);
