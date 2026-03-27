e08e8197d267d4b48a538a0ad977358f657d8178
Miguel <migueldco14@gmail.com>
2026-03-27 10:11:19 -0300

    adapt zeta

--- content/dsa/zeta.cpp
+++ content/dsa/zeta.cpp
@@ -1,35 +1,49 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-using VI = vector<int>;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+
+mt19937 rng(random_device{}());
 
 // begin template //
-VI subsetZeta(VI a) {
+vi subsetZeta(vi a) {
 	int n = a.size();
 	for (int j = 1; j < n; j <<= 1)
-		for (int i = 1; i < n; i++)
+		rep (i, 1, n)
 			if (i & j) a[i] += a[i ^ j];
 	return a;
 }
-VI subsetMobius(VI a) {
+vi subsetMobius(vi a) {
 	int n = a.size();
 	for (int j = 1; j < n; j <<= 1)
-		for (int i = 1; i < n; i++)
+		rep(i, 1, n)
 			if (i & j) a[i] -= a[i ^ j];
 	return a;
 }
-VI supersetZeta(VI a) {
+vi supersetZeta(vi a) {
 	int n = a.size();
 	for (int j = 1; j < n; j <<= 1)
-		for (int i = 1; i < n; i++)
+		rep(i, 1, n)
 			if (i & j) a[i ^ j] += a[i];
 	return a;
 }
-VI supersetMobius(VI a) {
+vi supersetMobius(vi a) {
 	int n = a.size();
 	for (int j = 1; j < n; j <<= 1)
-		for (int i = 1; i < n; i++)
+		rep(i, 1, n)
 			if (i & j) a[i ^ j] -= a[i];
 	return a;
 }
