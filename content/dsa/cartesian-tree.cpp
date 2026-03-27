a4245a70794abf0406e9371931937c3276281cf9
Miguel <migueldco14@gmail.com>
2026-03-27 09:57:11 -0300

    adapt cartesian tree to the new template

--- content/dsa/cartesian-tree.cpp
+++ content/dsa/cartesian-tree.cpp
@@ -1,16 +1,30 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-using VI = vector<int>;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define rep(i,a,b) for (int i = (a); i < (b); ++i)
+
+mt19937 rng(random_device{}());
 
 // begin template //
-VI cartTree(int n, auto le) {
+vi cartTree(int n, auto le) {
 	int last;
-	VI st, par(n);
-	for (int i = 0; i < n; i++){ 
+	vi st, par(n);
+	rep (i, 0, n) { 
 		par[i] = i, last = -1;
-		while (st.size() && le(par[i], st.back())) {
+		while (sz(st) && le(par[i], st.back())) {
 			if (last != -1) par[last] = st.back();
 			last = st.back();
 			st.pop_back();
@@ -18,7 +32,7 @@ VI cartTree(int n, auto le) {
 		if (last != -1) par[last] = i;
 		st.push_back(i);
 	}
-	while (st.size() > 1) {
+	while (sz(st) > 1) {
 		last = st.back();
 		st.pop_back();
 		par[last] = st.back();
