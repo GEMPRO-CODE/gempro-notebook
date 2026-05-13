d50b3f364c8add8e840a7f9c089f992bc39a8a5f
Miguel <migueldco14@gmail.com>
2026-05-13 11:22:57 -0300

    small fix

--- content/contest/gen.py
+++ content/contest/gen.py
@@ -1,5 +1,6 @@
 from random import randrange as rr, shuffle as sh, sample as sm, choice as ch, seed
 from string import ascii_lowercase as alpha
 from itertools import combinations as combs, permutations as perms, product as prod
+from sys import argv
 
 if len(argv) > 1: seed(argv[1])
